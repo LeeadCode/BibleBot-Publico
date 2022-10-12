275b315770b7dd7391ba0e28d84ca651d8f6154b
Lead <igoraugustoramos@gmail.com>
2022-10-11 21:31:03 -0300

    commit

---
+++ entidades/pokemonBot.py
@@ -0,0 +1,8 @@
+
+import discord
+
+class PokemonBot():
+    def __init__(self, nome):
+      self.id = 0
+      self.avatar_url = 'https://static.wikia.nocookie.net/meme/images/2/28/Stressed_Emoji.png/revision/latest?cb=20210814093341'
+      self.name = nome
